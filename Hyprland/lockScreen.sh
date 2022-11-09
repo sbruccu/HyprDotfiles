1ad1cb3807880e000e9edf0c1d9c3f8d1af48dbf
sbruccu <83294800+sbruccu@users.noreply.github.com>
2022-11-09 16:46:39 +0100

    Add files via upload

---
+++ Hyprland/lockScreen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+swaylock \
+	--screenshots \
+	--clock \
+	--indicator \
+	--indicator-radius 100 \
+	--indicator-thickness 7 \
+	--effect-blur 7x5 \
+	--effect-vignette 0.5:0.5 \
+	--ring-color  22304b\
+	--key-hl-color 70778a \
+	--line-color 00000000 \
+	--inside-color 00000088 \
+	--separator-color 00000000 \
+    --fade-in 0.2 \
+
+
